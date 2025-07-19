85d741ee2323a3d51036d076a6fcd9218dab6591
raghava rupesh <raghavarupesh@hotmail.com>
2025-07-19 15:41:42 +0530

    made linux_vm to linuxvm

--- application/apimxxxx1/outputs.tf
+++ application/apimxxxx1/outputs.tf
@@ -1,5 +1,5 @@
-output "linux_vm_id" {
-  value = { for k, m in module.linux_vm : k => m.vm_id }
+output "linuxvm_id" {
+  value = { for k, m in module.linuxvm : k => m.vm_id }
 }
 /*output "windows_vm_id" {
   value = module.windows_vm["windowsvm"].vm_id
